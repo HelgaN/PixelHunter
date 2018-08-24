10b5d7cedda3a71e2f2600ad2c4b793fb7881f04
HelgaN <ola14@tut.by>
2018-08-25 02:41:01 +0300

    #3

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const templates = document.querySelectorAll(`template`);
+const container = document.querySelector(`.central`);
+const content = container.innerHTML;
+
+const getElementFromTemplateNum = (templateNum) => {
+  container.innerHTML = templates[templateNum].innerHTML;
+  return container;
+}
+
+getElementFromTemplateNum(0);
+
+document.onkeydown = (evt) => {
+  if(evt.altKey && evt.key === `ArrowLeft`) {
+    evt.preventDefault();
+    getElementFromTemplateNum(0);
+  }
+  if(evt.altKey && evt.key === `ArrowRight`) {
+    getElementFromTemplateNum(1);
+  }
+}
